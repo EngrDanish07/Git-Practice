e00236a49d5a1b8510a348e257d099453724d2ac
Danish Hussain <danishh41149@gmail.com>
2022-10-17 00:21:10 +0500

    Assignment 1

---
+++ assignment1.js
@@ -0,0 +1,103 @@
+//Question 1
+//alert("Welcome to JS Land...\r\nHappy Coding!")
+
+//Question 2
+/*var name = "John Doe";
+var age = 20;
+var qualification = "Graduate";
+
+alert(name);
+alert(age);
+alert(qualification);*/
+
+//Question 3
+/*var email = "example@example.com"
+alert ("My email is " + email);*/
+
+//Question 4
+//document.write("Yah! I can write HTML content through Java Script")
+
+//Question 5
+/*var age = 20;
+alert("I am " + age + " years old");*/
+
+//Question 6
+/*var age = 2002;
+document.write("My birth year is " + age);
+document.write("<br>");
+document.write("Data type of my declared variable is " + typeof(age));*/
+
+//Question 7
+/*document.write("<h1>Rules for naming JS variables</h1> <br>");
+document.write("Variable names can only contain numbers, $ and _. ForExample: $my_1stVariable <br>");
+document.write("Variables must begin with a letter, $ or _. For example $name, _name or name <br>");
+document.write("Variable names are case sensative <br>");
+document.write("Variable names should not be JS keyword");*/
+
+//Question 8
+/*var a = 8;
+var b = 6;
+var c = a + b;
+document.write("Result of 8 + 6 = " + c);*/
+
+//Question 9
+/*var a = 8;
+var b = 6;
+var c = a - b;
+document.write("Result of 8 - 6 = " + c);*/
+
+//Question 10
+/*var a;
+document.write("Value after variable declaration is: " + typeof(a));
+a = 5;
+document.write("<br> Initial value is: " + a);
+a++;
+document.write("<br> Value after increment is: " + a);
+a = a + 7;
+document.write("<br> Value after adding 7 is: " + a);
+a--;
+document.write("<br> Value after deccrement is: " + a);
+document.write("<br> The remainder is: " + a%3);*/
+
+//Question 11 (Temperature Converter)
+/*var C = 25;
+var F = (C * 9/5) + 32;
+document.write("25 C is " + F + "F <br>");
+var F = 70;
+var C = (F - 32) * 5/9 ;
+document.write("70 F is " + C + "C");*/
+
+//Question 12
+/*document.write("<h1>Currency in PKR</h1>");
+document.write("Total currency: 10 USD & 25 Saudi Riyals");
+document.write("<p>1 USD = 218 <br>1 Saudi Riyal = 58 </p>");
+document.write("Total currency in PKR: " + ((10 * 218) + (25 * 58)) );*/
+
+//Question 13
+var a = 10;
+/*document.write("Result: <br>")
+document.write("The value of a is: " + a);
+document.write("<br> .................................<br><br><br>");
+document.write("The value of ++a is: " + ++a);
+document.write("<br>Now the value of a is: " + a);
+document.write("<br><br><br>")
+document.write("The value of a++ is: " + a++);
+document.write("<br>Now the value of a is: " + a);
+document.write("<br><br><br>")
+document.write("The value of --a is: " + --a);
+document.write("<br>Now the value of a is: " + a);
+document.write("<br><br><br>")
+document.write("The value of a-- is: " + a--);
+document.write("<br>Now the value of a is: " + a);
+document.write("<br><br><br>")*/
+
+//Question 14
+/*var a = 2; var b = 1;
+var result = --a - --b + ++b + b--;
+document.write("<br> The Equation: --a - --b + ++b + b-- <br>")
+document.write("Intialyy, <b>a</b> was 2 and <b>b</b> was 1<br>");
+document.write("<b>'--a'</b> When we pre decremented from a, it became: 1 <br>");
+document.write("<b>'--b'</b> When we pre decremented from b, it became: 0 <br>");
+document.write("And when subtracted --a - --b it becomes 1, because a was 1 and b was 0 <b>(--a - --b = 1)</b> <br>");
+document.write("Similarly, ++b = 2 and b-- = 1 <br>");
+document.write("Therefor the Result is: " + result);*/
